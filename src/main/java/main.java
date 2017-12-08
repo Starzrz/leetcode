3199b24e2b2a181f4b2d119512393934ad03c98c
Starzrz <151250213@smail.nju.edu.cn>
2017-12-08 08:03:55 +0800

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,26 @@
+/**
+ * Created by ${zrz} on 2017/12/7.
+ */
+public class main {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int one=0;
+            int two=0;
+            int []re = new int[2];
+            for(int i=0;i<nums.length;i++){
+                int t=target-nums[i];
+                for(int j=i+1;j<nums.length;j++){
+                    if(j==t){
+                        one= i;
+                        two=j;
+                        re[0]= one;
+                        re[1]= two;
+                        return re;
+                    }
+                }
+            }
+            return re;
+        }
+
+    }
+}
